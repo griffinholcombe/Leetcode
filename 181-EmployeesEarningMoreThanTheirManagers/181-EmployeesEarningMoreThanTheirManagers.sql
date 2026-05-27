dad2d4086593d06afca8825d4dec322d318ebc8b
Griffin <122371641+griffinholcombe@users.noreply.github.com>
2026-05-27 11:37:32 -0400

    Create 181-EmployeesEarningMoreThanTheirManagers

---
+++ 181-EmployeesEarningMoreThanTheirManagers/181-EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,9 @@
+-- Last updated: 5/27/2026, 11:37:31 AM
+# Write your MySQL query statement below
+SELECT e2.name as Employee
+FROM employee e1
+INNER JOIN employee e2 ON e1.id = e2.managerID
+WHERE 
+e1.salary < e2.salary
+
+
