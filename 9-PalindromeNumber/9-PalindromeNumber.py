2a55ea5c8058b94d7b4248ee08c2a8107cbd1f98
Griffin <122371641+griffinholcombe@users.noreply.github.com>
2026-07-06 11:14:44 -0400

    File updated at 7/6/2026, 11:14:44 AM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,23 +1,10 @@
-# Last updated: 7/6/2026, 11:09:21 AM
-1class Solution(object):
-2    def isPalindrome(self, x):
-3        """
-4        :type x: int
-5        :rtype: bool
-6        """
-7        if x < 0 or (x % 10 == 0 and x != 0):
-8            return False
+# Last updated: 7/6/2026, 11:14:44 AM
+1class Solution:
+2    def isPalindrome(self, x: int) -> bool:
+3        if x < 0:
+4            return False
+5        rev = int(str(x)[::-1])
+6        if rev == x:
+7            return True
+8        return False
 9        
-10        half = 0
-11
-12        while half < x:
-13            half = half * 10 + (x % 10)
-14            x //= 10
-15
-16        return half == x or half // 10 == x
-17        
-18
-19            
-20
-21            
-22        
